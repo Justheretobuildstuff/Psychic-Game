3331c26e08fb98c5910130e2ab16eef2ebe687b1
Josh <josh.daniels10@gmail.com>
2018-05-19 00:36:04 -0700

    closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,32 @@
 // Creating my variables
 var wins = 0;
 var losses = 0;
-var guessesleft = 0;
+var guessesleft = 9;
 var whatyouguessed = "";
-var chars = ["abcdefghijklmnopqurstuvwxyz"];
+var chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+    "l", "m", "n", "o", "p", "q", "u", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"];
 
 // Writing my function
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
     var userguess = event.key;
     var computerGuess = chars[Math.floor(Math.random() * chars.length)];
 
-    if (userguess === chars) {
-        if ((userguess === "a") && (computerGuess === "a")) {
+
+        if (userguess === computerGuess) {
             wins++;
         }
-    }
-    document.querySelector("#game").innerHTML = index.html;
-};
+
+        else 
+            console.log(computerGuess);
+                document.getElementById("letters").innerHTML = userguess;
+      
+                var html =
+                "<p>You chose: " + userGuess + "</p>" +
+                "<p>The computer chose: " + computerGuess + "</p>" +
+                "<p>wins: " + wins + "</p>" +
+                "<p>losses: " + losses + "</p>" +
+                "<p>ties: " + ties + "</p>";
+                document.querySelector("#game").innerHTML = html;
+    };
